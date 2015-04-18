052953e640ed1de7d62408544dd0d6614404e439
StefanPienaar <stefanp@fusiontec.co.za>
2015-04-18 02:12:45 +0200

    -

--- forum/viewtopic.php
+++ forum/viewtopic.php
@@ -5,6 +5,86 @@ include("../themes/crak/_header.php");
 
 ?>
 
+<div class="container">
+				<div class="row">
+					<div class="title_wrapper">
+						<div class="span6">
+							<h1>View Forum Post</h1>
+						</div>
+						<div class="breadcrumbs"><strong><a href="#"></a></strong></div>
+					</div>
+				</div>
+			</div>
 
+			<div class="page normal-page container">
+				<div class="row">
+					<div class="span12">
+						<div id="bbpress-forums">
+							<div class="bbp-template-notice info">
+								<p class="bbp-topic-description">This topic contains 0 replies, has 1 voice, and was last updated by <a href="#" title="View admin's profile" class="bbp-author-avatar" rel="nofollow"><img alt="" src="http://1.gravatar.com/avatar/38d93eff4c0db34aa79f07cf9ad1a89c?s=14&amp;d=http%3A%2F%2F1.gravatar.com%2Favatar%2Fad516503a11cd5ca435acc9bb6523536%3Fs%3D14&amp;r=G" class="avatar avatar-14 photo"></a>&nbsp;<a href="#" title="View admin's profile" class="bbp-author-name" rel="nofollow">admin</a> <a href="#" title="Curabitur sollicitudin mi vel auctor auctor">6 months ago</a>.</p>
+							</div>
+							<ul id="topic-993-replies" class="forums bbp-replies">
+								<li class="bbp-header">
+									<div class="bbp-reply-author">Author</div>
+									<!-- .bbp-reply-author -->
+									<div class="bbp-reply-content">
+										Posts
+									</div>
+									<!-- .bbp-reply-content -->
+								</li>
+								<!-- .bbp-header -->
+								<li class="bbp-body">
+									<div class="bbp-reply-header">
+										<div class="bbp-meta">
+											<span class="bbp-reply-post-date">August 30, 2013 at 1:46 pm</span>
+											<a href="#" class="bbp-reply-permalink">#993</a>
+											<span class="bbp-admin-links"></span>
+										</div>
+										<!-- .bbp-meta -->
+									</div>
+									<!-- #post-993 -->
+									<div class="post-993 topic type-topic status-publish hentry odd bbp-parent-forum-965 bbp-parent-topic-993 bbp-reply-position-1 user-id-1 topic-author instock">
+										<div class="bbp-reply-author">
+											<a href="#" title="View admin's profile" class="bbp-author-avatar" rel="nofollow"><img alt="" src="http://1.gravatar.com/avatar/38d93eff4c0db34aa79f07cf9ad1a89c?s=80&amp;d=http%3A%2F%2F1.gravatar.com%2Favatar%2Fad516503a11cd5ca435acc9bb6523536%3Fs%3D80&amp;r=G" class="avatar avatar-80 photo"></a><br><a href="#" title="View admin's profile" class="bbp-author-name" rel="nofollow">admin</a><br>
+											<div class="bbp-author-role">Keymaster</div>
+										</div>
+										<!-- .bbp-reply-author -->
+										<div class="bbp-reply-content">
+											<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tincidunt lacinia lacus, eget malesuada neque viverra et. Pellentesque malesuada urna ac magna dictum ultricies. Sed egestas magna eget urna porttitor, in tristique dui molestie. Nulla at rutrum est. Integer vitae neque ipsum. Phasellus volutpat nulla urna, eu mattis nibh egestas eget. Curabitur suscipit lectus a facilisis vulputate. Integer ut enim dictum, porttitor est id, placerat lorem. Maecenas quis vestibulum leo. Duis vulputate ut dui sed pretium.</p>
+											<p>Nulla vehicula, urna nec pretium elementum, lorem odio fringilla dolor, vitae volutpat metus urna eu augue. Aenean gravida dui elit, ut elementum elit pretium vitae. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque aliquam nibh eget nisi hendrerit, sit amet commodo sem aliquet. Sed tincidunt lorem lorem, in pellentesque velit auctor ut. Pellentesque imperdiet eros at ante dictum aliquam. Integer et nulla lobortis, tristique purus accumsan, sollicitudin dui. Curabitur sollicitudin mi vel auctor auctor. Pellentesque placerat tincidunt magna quis condimentum.</p>
+											<p>In hac habitasse platea dictumst. Duis malesuada imperdiet lacus sed bibendum. Vivamus sed purus quis enim feugiat laoreet quis eget arcu. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam et ante vel magna laoreet mattis sed ac mi. Donec id sollicitudin odio. Vestibulum nec ipsum convallis, sagittis sapien tincidunt, posuere arcu. Integer vitae malesuada nisi. Nullam venenatis, dolor quis blandit interdum, lorem erat porta nibh, lobortis tempor ligula nulla eu arcu. Cras quis nisi sit amet lacus sagittis eleifend vel eu quam. Proin tempor, lectus eget porta ullamcorper, sem lectus vulputate arcu, viverra lacinia ipsum lorem et lacus. Etiam sodales ante sit amet sagittis vestibulum. Morbi venenatis vestibulum aliquet. Suspendisse potenti.</p>
+										</div>
+										<!-- .bbp-reply-content -->
+									</div>
+									<!-- .reply -->
+								</li>
+								<!-- .bbp-body -->
+								<li class="bbp-footer">
+									<div class="bbp-reply-author">Author</div>
+									<div class="bbp-reply-content">
+										Posts
+									</div>
+									<!-- .bbp-reply-content -->
+								</li>
+								<!-- .bbp-footer -->
+							</ul>
+							<!-- #topic-993-replies -->
+							<div class="bbp-pagination">
+								<div class="bbp-pagination-count">
+									Viewing 1 post (of 1 total)
+								</div>
+								<div class="bbp-pagination-links">
+								</div>
+							</div>
+							<div id="no-reply-993" class="bbp-no-reply">
+								<div class="bbp-template-notice">
+									<p>You must be logged in to reply to this topic.</p>
+								</div>
+							</div>
+						</div>
+						<div class="clear"></div>
+					</div>
+				</div>
+			</div>
 
 <?php include("../themes/crak/_footer.php"); ?>
